0d08470d3387daa9b9b8750d96af0509838d167d
SONIA <etienne.b.pilon@gmail.com>
2014-10-24 12:42:18 -0400

    Added new media, media_template

---
+++ SeaGoatVision/server/media/implementation/ipc_sonar.py
@@ -0,0 +1,114 @@
+from Cython.Compiler.Errors import message
+
+__author__ = 'sonia'
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from SeaGoatVision.server.media.media_streaming import MediaStreaming
+from SeaGoatVision.commons.param import Param
+from SeaGoatVision.server.core.configuration import Configuration
+from SeaGoatVision.commons import log
+import thread
+import zmq
+import SonarImage
+import struct
+
+
+logger = log.get_logger(__name__)
+
+class IPC_Sonar(MediaStreaming):
+    """
+    Return image from IPC socket with ZeroMQ
+    This media is a subscriber of ZeroMQ
+    """
+    key_ipc_name = "sonar ipc name"
+
+    def __init__(self, config):
+        # Go into configuration/template_media for more information
+        super(IPC_Sonar, self).__init__()
+        self.config = Configuration()
+        self.own_config = config
+        self.media_name = config.name
+        self.sonarImage = SonarImage()
+        if config.device:
+            self.device_name = config.device
+        else:
+            self.device_name = "/tmp/seagoatvision_media.ipc_sonar"
+        self._is_opened = True
+        self.run = True
+        self.video = None
+
+        self.context = zmq.Context()
+        self.subscriber = None
+        self.message = None
+
+        self._create_params()
+        self.deserialize(self.config.read_media(self.get_name()))
+
+    def _create_params(self):
+        default_ipc_name = "ipc://%s" % self.device_name
+        self.param_ipc_name = Param(self.key_ipc_name, default_ipc_name)
+        self.param_ipc_name.add_notify(self.reload)
+
+    def open(self):
+        self.subscriber = self.context.socket(zmq.SUB)
+        self.subscriber.setsockopt(zmq.SUBSCRIBE, b'')
+        device_name = self.param_ipc_name.get()
+        logger.info("Open media device %s" % device_name)
+        self.subscriber.connect(device_name)
+        thread.start_new_thread(self.fill_message, tuple())
+        # call open when video is ready
+        return MediaStreaming.open(self)
+
+    def next(self):
+        if not self.subscriber or not self.message:
+            return
+        # Received messages are simply scanlines
+        # The formatting is the latter:
+        # Bytes:
+        # 0     Flag if the sonar configuration has changed
+        # 1-4   Angle min of the sweep (float)
+        # 5-8   Angle max of the sweep (float)
+        # 9-12  Angle Increment (float)
+        # 13-16 Number of Data per scanlines(int)
+        # 17-20 Scanline angle (middle of the scanline is 180.0)
+        # 21-*  Data (Value range from 0 to 255)
+
+        message = self.message[:]
+        self.message = None
+        lst_values = list(bytearray(message))
+
+        if message[0]:
+            self.sonarImage.initialize_Parameters(message[:20])
+
+        scanlineAngle = struct.pack('BBBB', message[17], message[18], message[19], message[20])
+        scanlineAngle = struct.unpack('f', scanlineAngle)
+
+        image = self.sonarImage.drawArcWithRange(message[21:], scanlineAngle)
+
+        if not image:
+            return
+
+        return image
+
+    def close(self):
+        MediaStreaming.close(self)
+        # TODO need to debug, closing socket create errors and \
+        # context.term freeze
+        # self.subscriber.close()
+        # self.context.term()
+        self.subscriber = None
+        return True
+
+    def fill_message(self):
+        try:
+            while self.subscriber:
+                self.message = self.subscriber.recv()
+        except zmq.ContextTerminated:
+            pass
+        finally:
+            self.message = None
---
+++ SeaGoatVision/server/media/implementation/sonar_image.py
@@ -0,0 +1,61 @@
+
+import numpy as np
+from PIL import Image
+import struct
+from PIL import ImageDraw
+
+
+class SonarImage():
+
+    def __init__(self):
+
+        #This is the width of the sonar's beam.
+        self.beamAperture = 5.0
+        self.threshold = 40
+        #used as a divider (3 == divided by 8)
+        self.bitShift = 3
+        #The image that will be passed to the filter chain
+        self.sonarImage = None
+        #The image workspace where new acquisitions will be made
+       # self.workspaceImage = None
+        self.imageCenter_x_y = None
+        self.stepAngleSize = None
+        self.numberOfBins = None
+        self.angleMin = None
+        self.angleMax = None
+       # self.arcPainter = None
+
+        self.newImageFlag = False
+
+    #array : https://docs.python.org/2/library/array.html
+    #def drawArcFromRange(self, range):
+
+    def initialize_Parameters(self, sonarScanline_msg):
+        dummy = None
+        dummy, self.angleMin, self.angleMax, self.stepAngleSize, self.numberOfBins, dummy = \
+            struct.unpack('BfffIf', sonarScanline_msg)
+        self.sonarImage = Image.new('1', (2*self.numberOfBins, self.numberOfBins), 1)
+      #  self.workspaceImage = Image.new('1', (2*self.numberOfBins, self.numberOfBins), 1)
+        self.imageCenter_x_y = (self.numberOfBins, self.numberOfBins)
+       # self.arcPainter = ImageDraw.Draw(self.workspaceImage)
+
+
+
+
+    def drawArcWithRange(self, scanline_data, angle):
+        data = list(bytearray[scanline_data])
+        workspaceImage = Image.new('1', (2*self.numberOfBins, self.numberOfBins), 1)
+        arcPainter = ImageDraw.Draw(workspaceImage)
+        for i in range(len(data)):
+            if data[i] > self.threshold:
+                box = (self.imageCenter_x_y[0] - i, self.imageCenter_x_y[1] - i,
+                       self.imageCenter_x_y[0] + i, self.imageCenter_x_y[1] + i)
+                arcPainter.arc(box, angle-self.beamAperture, angle+self.beamAperture, data[i] >> self.bitShift)
+
+
+        self.sonarImage = np.add(self.sonarImage, workspaceImage)
+
+        if angle == self.angleMax or angle == self.angleMin:
+            return self.sonarImage
+
+        return
